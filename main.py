1485d8e1a33483923ffb8cd1f225c528991c4104
ratmirasanov <ratmir.asanov.7@gmail.com>
2020-12-12 23:13:07 +0200

    Removed extra lines.

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import re
 import numpy as np
 import pandas as pd
-from sklearn import linear_model
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC, LinearSVC
-from sklearn.naive_bayes import GaussianNB
 
 
 test_df = pd.read_csv("test.csv")
